ea30020190c363911d5c98216e92d0ba5c71e67e
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-01-21 16:12:39 +0900

    Format by biome

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-    },
+	{
+		path: "/",
+		element: <Root />,
+	},
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,3 @@
 export default function Root() {
-    return (
-        <>
-            Logged in
-        </>
-    );
+	return <>Logged in</>;
 }
